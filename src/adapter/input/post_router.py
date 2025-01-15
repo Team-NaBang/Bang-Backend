4fa45ee0ef6b7ad5b9ecbf9e597b5ee1657e0539
bhyun08 <nbhyun0329@gmail.com>
2025-01-15 21:03:35 +0900

    ⚡️ refactor(#9): Enhance exception handling

--- src/adapter/input/post_router.py
+++ src/adapter/input/post_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, status, HTTPException
 from core.usecase import PostUseCase
 from adapter.output.post_repository_impl import PostReposiotryImpl
 from infrastructure.sqlalchemy.config import SessionLocal
@@ -17,6 +17,19 @@ def get_post_application_service():
     finally:
         db.close()
 
-@router.post(path='', status_code=status.HTTP_201_CREATED)
+@router.post(path='', 
+            status_code=status.HTTP_201_CREATED,
+            responses={
+                403: {"description": "인증 코드가 올바르지 않음"},
+                500: {"description": "서버 내부 오류"}
+            })
 def create_post(post: Post, service: PostApplicationService = Depends(get_post_application_service)):
-    return service.create_post(post)
+    try:
+        return service.create_post(post)
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
