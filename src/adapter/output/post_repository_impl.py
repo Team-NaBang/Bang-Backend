72725caa4d26f06b04dd00fc8209dd93be5acca4
bhyun08 <nbhyun0329@gmail.com>
2025-01-15 21:08:44 +0900

    🛠️ fix(#9): Strengthen database operation handling

--- src/adapter/output/post_repository_impl.py
+++ src/adapter/output/post_repository_impl.py
@@ -1,5 +1,6 @@
 from port.output.post_repository import PostRepository
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
 from core.domain import Post
 from infrastructure.sqlalchemy.model import Post as PostEntity
 from fastapi import HTTPException, status
@@ -10,6 +11,9 @@ load_dotenv()
 
 AUTHENTICATION_CODE = os.getenv("AUTHENTICATION_CODE")
 
+if not AUTHENTICATION_CODE:
+    raise ValueError("AUTHENTICATION_CODE 환경 변수가 설정되지 않았습니다.")
+
 class PostReposiotryImpl(PostRepository):
     def __init__(self, db:Session) -> None:
         self.db = db
@@ -18,9 +22,14 @@ class PostReposiotryImpl(PostRepository):
         if post.authentication_code != AUTHENTICATION_CODE:
             raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Authentication code not correct.")
         
-        post_entity = PostEntity(**post.model_dump(exclude={"authentication_code"}))
-        
-        self.db.add(post_entity)
-        self.db.commit()
-        self.db.refresh(post_entity)
-        return post_entity
+        try:
+            post_entity = PostEntity(**post.model_dump(exclude={"authentication_code"}))
+            self.db.add(post_entity)
+            self.db.commit()
+            self.db.refresh(post_entity)
+            return post_entity
+        except SQLAlchemyError as e:
+            self.db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="데이터베이스 작업 중 오류가 발생했습니다.") from e
