b7caa6c0095d9dd74f7758ebe2a062de4c11dac6
bhyun08 <nbhyun0329@gmail.com>
2025-01-16 21:47:46 +0900

    📝 style(#17): Add type hints for parameters

--- src/port/input/post_app_service.py
+++ src/port/input/post_app_service.py
@@ -9,11 +9,11 @@ class PostApplicationService:
     def create_post(self, post_create_request: PostCreateRequest) -> PostCreateResponse:
         return self.usecase.create_post(post_create_request)
     
-    def delete_post(self, post_id, authentication_code) -> None:
+    def delete_post(self, post_id:str, authentication_code) -> None:
         self.usecase.delete_post(post_id, authentication_code)
         
-    def update_post(self, post_id, post_update_request: PostUpdateRequest) -> PostUpdateResponse:
+    def update_post(self, post_id:str, post_update_request: PostUpdateRequest) -> PostUpdateResponse:
         return self.usecase.update_post(post_id, post_update_request)
     
-    def get_post_detail(self, post_id) -> PostDetailResponse:
+    def get_post_detail(self, post_id:str) -> PostDetailResponse:
         return self.usecase.get_post_detail(post_id)
